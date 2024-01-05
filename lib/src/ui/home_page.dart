cabe2a573030ccf6b06e81f99bc8aa978a7eb6ba
JatinderV <JatinderV@cidom.careington.com>
2024-01-05 15:10:11 +0530

    starter code

---
+++ lib/src/ui/home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: const Center(
+          child: Text('Multi Stepp App'),
+        ));
+  }
+}
